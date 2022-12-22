fb9c6fe2063cec1d06b7a1570230f58f040c2a11
Wellington Mitrut <wellingtonmitrut@gmail.com>
2022-12-22 15:16:50 -0300

    Christmas Update

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Image from 'gatsby-image'
-import { Instagram, GitHub, Linkedin } from 'react-feather';
+import { Instagram, GitHub, Linkedin, Hexagon } from 'react-feather'
 
 import { rhythm } from '../utils/typography'
 
@@ -13,39 +13,74 @@ function Bio() {
         const { author, social } = data.site.siteMetadata
         return (
           <>
-          <div
-            style={{
-              display: `flex`,
-            }}
-          >
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              alt={author}
-              
+            <div
               style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `0%`,
-                objectFit:`contain`
+                display: `flex`,
               }}
-            />
-            <p>
-              Blog escrito por
-              {` `}
-              <a href={`https://github.com/${social.twitter}`}>
-              <strong>{author}</strong>
-              </a> 
-              , Antes de tudo eu faço arte: código, música, fotografia, desenho e etc. Amo criar coisas
-              e compartilhar com o mundo
-            </p>
-            
-          </div>
-          <div className='social-button-area'>
-            <a href='https://www.instagram.com/wmitrut/' target='_blank' noref noopener alt="Instagram" className='social-button'><Instagram/></a>
-            <a href='https://github.com/Wmitrut' target='_blank' noref noopener alt="Instagram" className='social-button'><GitHub/></a>
-            <a href='https://www.linkedin.com/in/wmitrut/' target='_blank' noref noopener alt="Linkedin" className='social-button'><Linkedin/></a>
-          </div>
+            >
+              <Image
+                fixed={data.avatar.childImageSharp.fixed}
+                alt={author}
+                style={{
+                  marginRight: rhythm(1 / 2),
+                  marginBottom: 0,
+                  minWidth: 50,
+                  borderRadius: `0%`,
+                  objectFit: `contain`,
+                }}
+              />
+              <p>
+                Blog escrito por
+                {` `}
+                <a href={`https://github.com/${social.twitter}`}>
+                  <strong>{author}</strong>
+                </a>
+                , Antes de tudo eu faço arte: código, música, fotografia,
+                desenho e etc. Amo criar coisas e compartilhar com o mundo
+              </p>
+            </div>
+            <div className="social-button-area">
+              <a
+                href="https://www.instagram.com/wmitrut/"
+                target="_blank"
+                noref
+                noopener
+                alt="Instagram"
+                className="social-button"
+              >
+                <Instagram />
+              </a>
+              <a
+                href="https://github.com/Wmitrut"
+                target="_blank"
+                noref
+                noopener
+                alt="Instagram"
+                className="social-button"
+              >
+                <GitHub />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/wmitrut/"
+                target="_blank"
+                noref
+                noopener
+                alt="Linkedin"
+                className="social-button"
+              >
+                <Linkedin />
+              </a>
+              <a
+                href="https://mastodon.social/@wmitrut"
+                target="_blank"
+                noref
+                noopener
+                alt="Mastodon"
+                className="social-button"
+              >
+                <Hexagon />
+              </a>
+            </div>
           </>
         )
       }}
